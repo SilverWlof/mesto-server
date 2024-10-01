878004003654dfbc9a56209894c5890391a1cd60
Mikhail <33647581+SilverWlof@users.noreply.github.com>
2024-10-01 23:22:58 +0400

    Update auth.ts

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,9 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 
 export const auth = (req: Request, res: Response, next: NextFunction) => {
-  // Эта часть не работает, как и не работает req.user = {_id: '662d16b824e7ed721daa1315'}
+  // Эта часть не работает, как и не работает req.user = {_id: '6ad1c47f-6af4-42c4-b56f-bacd15588390'}
   req.params = {
-    _id: '662d16b824e7ed721daa1315'
+    _id: '6ad1c47f-6af4-42c4-b56f-bacd15588390'
   }
 
   next();
